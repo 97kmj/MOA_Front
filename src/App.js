5b54af128ce51f6231f80438c9cfd3f6f5bb293f
97kmj <kmj0376@gmail.com>
2024-11-11 16:48:40 +0900

    11-11commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Header from './views/Header';
 import Main from './views/user/Main';
+import Notice from './views/user/Notice';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Notice/>
       {/* <div>
         <Routes>
           <Route exect path='/' element={<Main/>}/>
