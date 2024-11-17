cd501c51d3ab3e29580270bfa679a3a4499eabad
김민준 <kmj0376@gmail.com>
2024-11-18 00:00:06 +0900

    mypage-artist, admin페이지

---
+++ src/views/admin/AdminQnA.js
@@ -0,0 +1,224 @@
+import AdminSidebar from "./AdminSidebar";
+import styles from "../../css/admin/adminQnA.module.css"
+const AdminQnA = () => {
+    return(
+        
+        <>
+        <div className={styles.container}>
+            <AdminSidebar/>
+            <div className={styles.questionList}>
+                <h4>답변 완료한 문의</h4>
+                <hr/>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className={styles.questionList}>
+                <h4>답변 대기중 문의</h4>
+                <hr/>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.title}>작가 회원 신청 시 유의사항</td>
+                        <td className={styles.date}>2024-10-22</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className={styles.questionDetail}>
+                <h4>문의 상세</h4>
+                <div className={styles.detailTitle}>제목</div><input   readOnly />
+                <div className={styles.detailContent}>내용</div>
+                <textarea></textarea>
+                <hr/>
+                <h4>답변 작성</h4>
+                <div className={styles.detailTitle}>제목</div><input   readOnly />
+                <div className={styles.detailContent}>내용</div>
+                <textarea></textarea>
+                <div className={styles.buttonDiv}>
+                    <button className={styles.goldbutton}>답변하기</button>
+                </div>
+
+            </div>
+        </div>
+        </>
+        
+    )
+}
+ 
+export default AdminQnA;
